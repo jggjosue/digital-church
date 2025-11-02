caeec9664e2e308fcf0cf340fc784923b9f1e7d2
Josue Gonzalez <jggjosue@gmail.com>
2025-11-02 04:36:20 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,12 +18,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
           inter.variable
         )}
+        suppressHydrationWarning
       >
         <div className="flex min-h-screen w-full flex-col bg-background text-foreground">
           <div className="flex">
